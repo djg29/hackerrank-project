2262a8c49fc57d09a37792a75aeab9d3aa4a7ee0
dgogo1 <dhrubajyoti.gogoi@nike.com>
2020-01-31 10:55:55 +0100

    rearranging order

--- src/main/scala/com/nike/interview/controller/StockTradeApiController.scala
+++ src/main/scala/com/nike/interview/controller/StockTradeApiController.scala
@@ -1,10 +1,31 @@
 package com.nike.interview.controller
 
-import com.nike.interview.repository.{ItemRepository}
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import com.nike.interview.model.Trade
+import com.nike.interview.repository.ItemRepository
 
 case class StockTradeApiController(repo: ItemRepository) {
 
-  val route = ???
+  val route = concat(
+    get {
+      pathPrefix("trades") {
+        concat(
+          path("users" / IntNumber) { userId =>
+            complete(StatusCodes.NotFound)
+          }
+        )
+      }
+    },
+    post {
+      path("trades") {
+        entity(as[Trade]) { transcation =>
+          complete(StatusCodes.NotFound)
+        }
+      }
+    }
+  )
 
 
 }
